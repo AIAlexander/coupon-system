ba17702ed87f2b48cdace2a07fab8f1aaae48b85
alex <5791159@qq.com>
2020-06-07 23:38:35 +0800

    商户端添加拦截器

--- merchants/src/main/java/com/alex/passbook/MerchantsApplication.java
+++ merchants/src/main/java/com/alex/passbook/MerchantsApplication.java
@@ -1,16 +1,31 @@
 package com.alex.passbook;
 
+import com.alex.passbook.security.AuthCheckInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.annotation.Resource;
 
 /**
  * @author wsh
  * @date 2020-06-07
  */
 @SpringBootApplication
-public class MerchantsApplication {
+public class MerchantsApplication extends WebMvcConfigurerAdapter {
+
+    @Resource
+    private AuthCheckInterceptor authCheckInterceptor;
+
     public static void main(String[] args) {
         SpringApplication.run(MerchantsApplication.class, args);
+    }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(authCheckInterceptor)
+                .addPathPatterns("/merchants/**");
+        super.addInterceptors(registry);
     }
 }
